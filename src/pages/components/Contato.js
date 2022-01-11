488841b9d169c74e09a4a292c41e8ce64cfd2bd9
Cristian Soares <criricyt@gmail.com>
2022-01-11 20:39:40 -0300

    rel

--- src/pages/components/Contato.js
+++ src/pages/components/Contato.js
@@ -12,22 +12,30 @@ function Contato() {
       <p>Para entrar em contato, acesse nossas redes sociais</p>
       <div className={styles.social}>
         <div className={styles.facebook}>
-          <a href="https://facebook.com" target="_blank">
+          <a href="https://facebook.com" target="_blank" rel="noreferrer">
             <GrFacebookOption />
           </a>
         </div>
         <div className={styles.insta}>
-          <a href="https://instagram.com/soarescristian_" target="_blank">
+          <a
+            href="https://instagram.com/soarescristian_"
+            target="_blank"
+            rel="noreferrer"
+          >
             <GrInstagram />
           </a>
         </div>
         <div className={styles.linkedin}>
-          <a href="https://www.linkedin.com/in/cristian-soares" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/cristian-soares"
+            target="_blank"
+            rel="noreferrer"
+          >
             <GrLinkedinOption />
           </a>
         </div>
         <div className={styles.github}>
-          <a href="https://github.com/criric" target="_blank">
+          <a href="https://github.com/criric" target="_blank" rel="noreferrer">
             <GrGithub />
           </a>
         </div>
